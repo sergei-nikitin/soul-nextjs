6ee1216151975fdff2ba6161456d7fb03ff4e991
Gennadii <atlantagenafes@gmail.com>
2022-07-26 10:30:27 +0300

    change contacts and seduction soul import

--- pages/contacts.js
+++ pages/contacts.js
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react';
 import { MainLayout } from '../components/mainLayout/MainLayout';
 import Main from '../components/pageContacts/main/Main';
 import FormSection from '../components/pageContacts/form/FormSection';
-import { Soul } from '../components/home/soul/Soul';
+import Soul from '../components/home/soul/Soul';
 
 export default function Contacts() {
   useEffect(() => {
